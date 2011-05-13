3ea99ff2290717f50d1f92cee7bcc0d842982b9f
Brian Sam-Bodden <bsbodden@bsbmbp.local>
2011-05-13 11:25:06 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyazOrg::Application.config.secret_token = 'bb840d6947fd1a1e2b9fa5172cc3297207d3b6ba6c1155113d1c42f7da1de48c230bc547228c6d1e31a26bca14c79812010a060b18d7b40e928663e462c3c25d'
